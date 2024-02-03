2dc4daacdce51262dad14531f320ca25febd82dc
JacobRyanHanson <jacobryanhanson@gmail.com>
2024-02-03 09:10:11 -0600

    Update db

--- models/seed.py
+++ models/seed.py
@@ -15,11 +15,39 @@ def seed():
     useDB = "USE fitness_progress_tracker;"
     pool.execute(useDB)
 
-    createUserTable = """CREATE TABLE User (
+    createUserTable = """CREATE TABLE user (
                             UserID INT AUTO_INCREMENT PRIMARY KEY,
-                            Email VARCHAR(255),
+                            Email VARCHAR(255) UNIQUE,
                             PasswordHash VARCHAR(255)
                         );"""
     pool.execute(createUserTable)
+
+    createUserProfileTable = """CREATE TABLE user_profile (
+                                ProfileID INT AUTO_INCREMENT PRIMARY KEY,
+                                UserID INT,
+                                FirstName VARCHAR(255),
+                                LastName VARCHAR(255),
+                                Age INT,
+                                Gender VARCHAR(50),
+                                ProfileVisibility ENUM('PUBLIC', 'PRIVATE'),
+                                Bio TEXT,
+                                Avatar BLOB,
+                                FOREIGN KEY (UserID) REFERENCES user(UserID) ON DELETE CASCADE
+                            );"""
+    pool.execute(createUserProfileTable)
+
+    createRolesTable = """CREATE TABLE role (
+                            RoleID INT AUTO_INCREMENT PRIMARY KEY,
+                            RoleName VARCHAR(255)
+                        );"""
+    pool.execute(createRolesTable)
+
+    createUserRolesTable = """CREATE TABLE user_role (
+                              UserID INT,
+                              RoleID INT,
+                              FOREIGN KEY (UserID) REFERENCES user(UserID) ON DELETE CASCADE,
+                              FOREIGN KEY (RoleID) REFERENCES role(RoleID) ON DELETE CASCADE
+                          );"""
+    pool.execute(createUserRolesTable)
 
 seed()
