6ff5a51fd202ed093ccda083fc8d976ce89ca371
JacobRyanHanson <jacobryanhanson@gmail.com>
2024-02-03 14:05:32 -0600

    Update user

--- resources/user.py
+++ resources/user.py
@@ -4,22 +4,29 @@ from models.ConnectionPool import ConnectionPool
 class Users(Resource):
     def get(self):
         pool = ConnectionPool()
+
         query = "SELECT user_id, email FROM user"
-        results = pool.execute(query)
-        return {'users': results}, 200
+        result = pool.execute(query)
+
+        if result['message']:
+            return {'message': result['message']}, 500
+        
+        return {'users': result["rows"]}, 200
 
 class UserById(Resource):
     def get(self, user_id):
         pool = ConnectionPool()
-        query = "SELECT user_id, email FROM user WHERE user_id = %s"
-        results = pool.execute(query, (user_id,))
-        if len(results) == 1:
-            return {'users': results}, 200
-        elif len(results) == 0:
-            return {'message': f'User with UserID {user_id} not found'}, 404
-        else:
-            return {'message': 'Internal Server Error'}, 500
 
+        query = "SELECT user_id, email FROM user WHERE user_id = %s"
+        result = pool.execute(query, (user_id,))
+
+        if result['message']:
+            return {'message': result['message']}, 500
+        
+        if result['rows']:
+            return {'users': result["rows"]}, 200
+        else:
+            return {'message': f'User with UserID {user_id} not found'}, 404
 
         
 
